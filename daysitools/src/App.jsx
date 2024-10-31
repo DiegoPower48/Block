9a92fafb39bd076a942e1b825a34747780cd2105
Diego <diego_torres_11@hotmail.com>
2024-10-30 20:16:26 -0500

    finishing ui

--- daysitools/src/App.jsx
+++ daysitools/src/App.jsx
@@ -1,6 +1,46 @@
 import "./App.css";
 
 function App() {
+  // const { register, watch, handleSubmit, reset } = useForm();
+
+  // const informacionFormulario = watch();
+
+  // const Datos = () => {
+  //   toast
+  //     .promise(
+  //       axios.post(
+  //         "https://portfolio-c4l9.onrender.com/correo",
+  //         informacionFormulario
+  //       ),
+  //       {
+  //         loading: "⏳⏳  ENVIANDO COMENTARIO......",
+  //         success: <b>"GRACIAS POR EL COMENTARIO!!!!🚀"</b>,
+  //         error: <b>NO SE PUDO GUARDAR</b>,
+  //       }
+  //     )
+  //     .then((response) => {
+  //       reset();
+  //       console.log(response);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
+
+  // return (
+  //   <form className="comentarios" id="Contactos" method="POST">
+  //     <fieldset className="contactame">
+  //       <h1 className="titulo">CONTACT ME:</h1>
+  //     </fieldset>
+  //     <fieldset className="commentsField">
+  //       <input
+  //         id="nombre"
+  //         type="text"
+  //         className="formulario"
+  //         {...register("nombre", { required: true })}
+  //         required
+  //       />
+
   return (
     <>
       <div className="container">
