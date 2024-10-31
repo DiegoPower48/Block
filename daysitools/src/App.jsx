658062a23376f044948efa864f7128397845990a
Diego <diego_torres_11@hotmail.com>
2024-10-30 19:37:35 -0500

    first commit

---
+++ daysitools/src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <textarea
+          className="cajitaxd"
+          spellcheck="false"
+          type="textArea"
+        ></textarea>
+        <textarea
+          spellcheck="false"
+          className="cajitaxd"
+          type="textArea"
+        ></textarea>
+        <textarea
+          className="cajitaxd"
+          spellcheck="false"
+          type="textArea"
+        ></textarea>
+        <textarea
+          className="cajitaxd"
+          spellcheck="false"
+          type="textArea"
+        ></textarea>
+        <textarea
+          className="cajitaxd"
+          spellcheck="false"
+          type="textArea"
+        ></textarea>
+        <textarea
+          className="cajitaxd"
+          spellcheck="false"
+          type="textArea"
+        ></textarea>
+        <textarea
+          className="cajitaxd"
+          spellcheck="false"
+          type="textArea"
+        ></textarea>
+        <textarea
+          className="cajitaxd"
+          spellcheck="false"
+          type="textArea"
+        ></textarea>
+      </div>
+    </>
+  );
+}
+
+export default App;
