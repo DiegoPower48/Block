07907ed7d722d0a0fb0a514bb0bf53522a6ea5c1
Diego <diego_torres_11@hotmail.com>
2024-11-08 02:16:06 -0500

    agregando alerta

--- daysitools/src/App.jsx
+++ daysitools/src/App.jsx
@@ -1,7 +1,38 @@
+import { useEffect } from "react";
 import "./App.css";
 import Cuadro from "./components/form";
 
 function App() {
+  const temporizador = () => {
+    try {
+      const tiempo = parseFloat(
+        prompt("♥♥♥♥♥♥BUEN DIA HERMOSA A QUE HORA TERMINAS ♥♥♥♥♥♥:")
+      );
+      const ahora = new Date();
+      const final = new Date();
+      final.setHours(tiempo, 0, 0);
+
+      if (ahora.getHours() > final.getHours()) {
+        final.setHours(tiempo + 11, 55, 0);
+      } else {
+        final.setHours(tiempo - 1, 55, 0);
+      }
+
+      const temporizador = final - ahora;
+
+      console.log(final, ahora, temporizador);
+      setTimeout(() => {
+        alert(`TU TURNO TERMINA A LAS ${tiempo} ATENTA, EN 5 MINUTOS!!! ♥♥♥♥ `);
+      }, temporizador);
+    } catch (error) {
+      console.log("error, no se ejecutó el temporizador, ES CULPA DE DIEGO");
+    }
+  };
+
+  useEffect(() => {
+    temporizador();
+  }, []);
+
   return (
     <>
       <form className="container">
